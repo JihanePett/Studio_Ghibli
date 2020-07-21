6b86ee5bc2ba44d42efc517b537db3e6a325d5ac
JihanePett <jihane.petticrew@gmail.com>
2020-07-21 14:30:36 +0000

    data.js code update to populate the json data into modal and  index.html updated with modal form from bootstrap

--- js/data.js
+++ js/data.js
@@ -1,18 +1,77 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://ghibliapi.herokuapp.com/films/");
-xhr.send();
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", "https://ghibliapi.herokuapp.com/films/");
+//   xhr.send();
 
-xhr.onload = function () {
-  var data = JSON.parse(this.response);
+//   xhr.onload = function () {
+//     var data = JSON.parse(this.response);
 
-  if (this.readyState == 4 && this.status == 200) {
-    var linkout = "";
-    var i;
-    for(i = 0; i < data.length; i++) {
-        linkout += '<a class="film-url" target="_blank" onclick="window.open(href, width=100,height=100)" href="' + data[i].url + '">' +
-        data[i].title + '</a><br>';
+//     if (this.readyState == 4 && this.status == 200) {
+//       var linkout = "";
+//       var i;
+//       for (i = 0; i < data.length; i++) {
+//         linkout += `<a class="film-url" target="_blank" onclick="populatemodal('${data[i].url}')>${data[i].title} + '</a><br>`;
+//       }
+
+//       document.getElementById("placeholder").innerHTML = linkout;
+//     }
+//   }
+// });
+
+const urls = [];
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://ghibliapi.herokuapp.com/films/");
+  xhr.send();
+
+  xhr.onload = function () {
+    var data = JSON.parse(this.response);
+
+    if (this.readyState == 4 && this.status == 200) {
+      var linkout = "";
+      var i;
+      for (i = 0; i < data.length; i++) {
+        linkout +=
+          `<a class="film-url" href="javascript:populatemodal('${
+            data[i].url
+          }', '${data[i].title.replace(/'/gi, "")}')">` +
+          data[i].title +
+          "</a><br>";
+      }
+
+      document.getElementById("placeholder").innerHTML = linkout;
     }
+  };
+});
 
-    document.getElementById("placeholder").innerHTML = linkout;
+const getdata = async function () {
+  const arrayOfPromises = urls.map((url) => axios.get(url[0]));
+  for await (let request of arrayOfPromises) {
+    $("#modalBody").text(JSON.stringify(request.data));
+  }
+};
+
+async function populatemodal(url, title) {
+  // set the modal title
+  $("#staticBackdropLabel").html(title);
+
+  // Get the data from the selected URL
+  const response = await axios.get(url);
+
+  await buildView(response.data.people, 'People');
+  await buildView(response.data.species, 'Species');
+  await buildView(response.data.locations, 'Locations');
+  await buildView(response.data.vehicles, 'Vehicles');
+
+  // Show Modalspecies
+  $("#staticBackdrop").modal("toggle");
 }
+
+async function buildView(array_data, header) {
+  const arrayOfPromises = array_data.map((url) => axios.get(url));
+  for await (let response of arrayOfPromises) {
+    // $("#modalBody").text(JSON.stringify(response.data));
+    $(`<p><h3>${header}</h3>${JSON.stringify(response.data)}</p>`).appendTo('#modalBody');
+  }
 }
